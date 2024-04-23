adc99c1feef939e66cad4e449647200ebb49dace
Stephen Reindl <stephen.reindl@outlook.de>
2024-04-23 16:20:31 +0200

    chore: package upgrades

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,12 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
 
+// eslint-disable-next-line no-undef
 const dev = process.env.ROLLUP_WATCH;
 
 const serveopts = {
@@ -22,14 +23,14 @@ const serveopts = {
 const plugins = [
   nodeResolve({sourcemap: false}),
   commonjs({sourcemap: false}),
-  typescript({
-    sourcemap: false,
-    typescript: require('typescript'),
-    objectHashIgnoreUnknownHack: true
-  }),
+  typescript(),
   json({sourcemap: false}),
   babel({
-    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+    exclude: [
+      'node_modules/**',
+      '*.mjs'
+    ],
     sourcemap: false
   }),
   dev && serve(serveopts),
